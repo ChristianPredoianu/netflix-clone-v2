d020b60e21e3fdfd175fa8e24ed95dad5affb167
ChristianPredoianu <50207986+ChristianPredoianu@users.noreply.github.com>
2022-02-04 13:17:58 +0100

    imported icons file + .use(router) .use(store) .component(font-awesome-idon)

--- src/main.js
+++ src/main.js
@@ -1,5 +1,19 @@
 import { createApp } from 'vue';
 import App from './App.vue';
+import router from './router/index';
+import store from './store';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import addIcons from './font-awesome-icons/icons';
+
 import './sass/main.scss';
 
-createApp(App).mount('#app');
+addIcons();
+
+//This is how to use font awesome
+/* <font-awesome-icon icon="laptop" /> */
+
+createApp(App)
+  .use(router)
+  .use(store)
+  .component('font-awesome-icon', FontAwesomeIcon)
+  .mount('#app');
