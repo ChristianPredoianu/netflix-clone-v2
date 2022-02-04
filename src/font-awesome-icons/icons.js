379f5a29488b7cf5df57cc1c0081d21bf205bbc2
ChristianPredoianu <50207986+ChristianPredoianu@users.noreply.github.com>
2022-02-04 13:20:36 +0100

    imported icons in separate file to keep main.js lean

---
+++ src/font-awesome-icons/icons.js
@@ -0,0 +1,51 @@
+import { library } from '@fortawesome/fontawesome-svg-core';
+
+import {
+  faLaptop,
+  faTv,
+  faMobileAlt,
+  faPlusSquare,
+  faSmile,
+  faFlushed,
+  faGrinTongueWink,
+  faGrinTears,
+  faEdit,
+  faPlay,
+  faInfoCircle,
+  faVolumeMute,
+  faVolumeUp,
+  faHistory,
+  faPlayCircle,
+  faPlusCircle,
+  faTimesCircle,
+  faSearch,
+  faAngleDown,
+  faTimes,
+} from '@fortawesome/free-solid-svg-icons';
+
+const addIcons = () => {
+  library.add(
+    faLaptop,
+    faTv,
+    faMobileAlt,
+    faPlusSquare,
+    faSmile,
+    faFlushed,
+    faGrinTongueWink,
+    faGrinTears,
+    faEdit,
+    faPlay,
+    faInfoCircle,
+    faVolumeMute,
+    faVolumeUp,
+    faHistory,
+    faPlayCircle,
+    faPlusCircle,
+    faTimesCircle,
+    faSearch,
+    faAngleDown,
+    faTimes
+  );
+};
+
+export default addIcons;
