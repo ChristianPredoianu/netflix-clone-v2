ed7e7e73c151b856901ea98f8972c13a89b10ed4
ChristianPredoianu <50207986+ChristianPredoianu@users.noreply.github.com>
2022-02-04 13:30:59 +0100

    added views folder

---
+++ src/views/Home.vue
@@ -0,0 +1,12 @@
+<script setup>
+import classes from '../sass/views/Home.module.scss';
+</script>
+
+<template>
+  <div>
+    <h1>dsad</h1>
+    <font-awesome-icon icon="laptop" />
+  </div>
+</template>
+
+<style lang="scss" scoped></style>
