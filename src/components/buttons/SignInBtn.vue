3a4b694d6bfd84c81af79a1db14fbff7ade8c660
ChristianPredoianu <50207986+ChristianPredoianu@users.noreply.github.com>
2022-02-07 19:33:34 +0100

    changed file name

---
+++ src/components/buttons/SignInBtn.vue
@@ -0,0 +1,13 @@
+<script setup></script>
+
+<template>
+  <div>
+    <button :class="classes.logInBtn">
+      <slot></slot>
+    </button>
+  </div>
+</template>
+
+<style lang="scss" module="classes">
+@use '@/sass/components/buttons/sign-in-btn';
+</style>
