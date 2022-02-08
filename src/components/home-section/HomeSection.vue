fbb25c83ad6aaf2e21323a94fa5edd8884716ea2
ChristianPredoianu <50207986+ChristianPredoianu@users.noreply.github.com>
2022-02-08 15:43:18 +0100

    added home section components

---
+++ src/components/home-section/HomeSection.vue
@@ -0,0 +1,11 @@
+<script setup></script>
+
+<template>
+  <section :class="classes.homeSection">
+    <slot></slot>
+  </section>
+</template>
+
+<style lang="scss" module="classes">
+@use '@/sass/components/home-section/home-section';
+</style>
---
+++ src/components/home-section/HomeSectionHeadings.vue
@@ -0,0 +1,17 @@
+<script setup>
+const props = defineProps({
+  heading: String,
+  subHeading: String,
+});
+</script>
+
+<template>
+  <div :class="classes.homeSectionTextContainer">
+    <h2 :class="classes.homeSectionHeading">{{ props.heading }}</h2>
+    <h3 :class="classes.homeSectionSubHeading">{{ props.subHeading }}</h3>
+  </div>
+</template>
+
+<style lang="scss" module="classes">
+@use '@/sass/components/home-section/home-section-headings';
+</style>
