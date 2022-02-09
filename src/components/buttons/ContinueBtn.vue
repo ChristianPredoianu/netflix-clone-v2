f7b2fa7c61e8bc8fc1f6d6735f7fe029ef10d06b
ChristianPredoianu <50207986+ChristianPredoianu@users.noreply.github.com>
2022-02-09 16:03:15 +0100

    added reusable button

---
+++ src/components/buttons/ContinueBtn.vue
@@ -0,0 +1,11 @@
+<script setup></script>
+
+<template>
+  <div>
+    <button type="submit" :class="classes.continueBtn"><slot></slot></button>
+  </div>
+</template>
+
+<style lang="scss" module="classes">
+@use '@/sass/components/buttons/continue-btn';
+</style>
