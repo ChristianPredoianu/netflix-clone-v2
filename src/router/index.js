08d77d9b9d9ee5a1334a06949f959413761ec329
ChristianPredoianu <50207986+ChristianPredoianu@users.noreply.github.com>
2022-02-09 16:01:14 +0100

    added some routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import Home from '../views/Home.vue';
+import Home from '@/views/Home.vue';
 const routes = [
   {
     path: '/',
@@ -8,9 +8,27 @@ const routes = [
   },
 
   {
+    path: '/signin',
+    name: 'SignIn',
+    component: () => import('@/views/SignIn.vue'),
+  },
+
+  {
     path: '/browse',
     name: 'Browse',
-    component: () => import('../views/Browse.vue'),
+    component: () => import('@/views/Browse.vue'),
+  },
+
+  {
+    path: '/signup/step1',
+    name: 'SignupStep1',
+    component: () => import('@/views/signup/Step1.vue'),
+  },
+
+  {
+    path: '/signup/step2',
+    name: 'SignupStep2',
+    component: () => import('@/views/signup/Step2.vue'),
   },
 ];
 const router = createRouter({
