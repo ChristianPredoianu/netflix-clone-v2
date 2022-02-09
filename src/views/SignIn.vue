afa881946cad843e2c27a1ec240b30fefaa0d3ef
ChristianPredoianu <50207986+ChristianPredoianu@users.noreply.github.com>
2022-02-09 16:06:32 +0100

    added sign in view

---
+++ src/views/SignIn.vue
@@ -0,0 +1,27 @@
+<script setup>
+import NetflixLogo from '@/components/ui/NetflixLogo.vue';
+import SignInForm from '@/components/forms/SignInForm.vue';
+</script>
+
+<template>
+  <div :class="classes.signIn">
+    <div :class="classes.overlay"></div>
+    <div class="container">
+      <header :class="classes.header">
+        <nav>
+          <NetflixLogo />
+        </nav>
+      </header>
+
+      <main :class="classes.main">
+        <div :class="classes.signInForm">
+          <SignInForm />
+        </div>
+      </main>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" module="classes">
+@use '@/sass/views/sign-in';
+</style>
