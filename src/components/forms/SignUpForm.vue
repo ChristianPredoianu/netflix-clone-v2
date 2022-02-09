6b23689b979507b283bc4a23bda33516c6a4cf79
ChristianPredoianu <50207986+ChristianPredoianu@users.noreply.github.com>
2022-02-09 16:03:49 +0100

    added sing up form

---
+++ src/components/forms/SignUpForm.vue
@@ -0,0 +1,34 @@
+<script setup>
+import ContinueBtn from '@/components/buttons/ContinueBtn.vue';
+function signUp() {
+  console.log('Signing up..');
+}
+</script>
+
+<template>
+  <div>
+    <label for="Email">Email:</label>
+    <form @submit.prevent="signUp">
+      <input
+        type="email"
+        placeholder="E-mail"
+        required
+        :class="classes.formInput"
+      />
+      <label for="Email">Password:</label>
+      <input
+        type="password"
+        placeholder="Password"
+        minlength="6"
+        maxlength="60"
+        required
+        :class="classes.formInput"
+      />
+      <ContinueBtn>Continue</ContinueBtn>
+    </form>
+  </div>
+</template>
+
+<style lang="scss" module="classes">
+@use '@/sass/components/forms/sign-up-form';
+</style>
