d03b4358224c1a5482c620e549e03e2477cad6e0
ChristianPredoianu <50207986+ChristianPredoianu@users.noreply.github.com>
2022-02-10 14:48:58 +0100

    added profile btn component

---
+++ src/components/buttons/ProfilesBtn.vue
@@ -0,0 +1,8 @@
+<script setup></script>
+<template>
+  <button :class="classes.profilesBtn"><slot></slot></button>
+</template>
+
+<style lang="scss" module="classes">
+@use '@/sass/components/buttons/profiles-btn';
+</style>
