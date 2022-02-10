cefd6ebb89e822ae9b39e2fe9b42e93cb7529d73
ChristianPredoianu <50207986+ChristianPredoianu@users.noreply.github.com>
2022-02-10 14:49:13 +0100

    added logo nav component

---
+++ src/components/navs/LogoNav.vue
@@ -0,0 +1,17 @@
+<script setup>
+import NetflixLogo from '@/components/ui/NetflixLogo.vue';
+</script>
+
+<template>
+  <div>
+    <header :class="classes.header">
+      <nav>
+        <NetflixLogo />
+      </nav>
+    </header>
+  </div>
+</template>
+
+<style lang="scss" module="classes">
+@use '@/sass/components/navs/logo-nav';
+</style>
