b9ac035754620f7c1a5d7cd93928f73e94785c19
ChristianPredoianu <50207986+ChristianPredoianu@users.noreply.github.com>
2022-02-10 14:52:16 +0100

    added profiles view elements

---
+++ src/views/Profiles.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { shallowRef } from 'vue';
+import LogoNav from '@/components/navs/LogoNav.vue';
+import UserProfiles from '@/components/profiles/UserProfiles.vue';
+
+let currentComponent = shallowRef(UserProfiles);
+
+function changeComponent(comp) {
+  currentComponent.value = comp;
+}
+</script>
+
+<template>
+  <div :class="classes.profiles">
+    <LogoNav />
+    <div class="container">
+      <transition name="fade">
+        <component :is="currentComponent" @change-component="changeComponent" />
+      </transition>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" module="classes">
+@use '@/sass/views/profiles';
+</style>
