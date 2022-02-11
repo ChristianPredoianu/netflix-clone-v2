46c0d321d3263a0d903fba2e56d95191dcb6fbc4
ChristianPredoianu <50207986+ChristianPredoianu@users.noreply.github.com>
2022-02-11 10:16:46 +0100

    added elements

--- src/components/profiles/ManageProfiles.vue
+++ src/components/profiles/ManageProfiles.vue
@@ -1,5 +1,6 @@
 <script setup>
 import UserProfiles from '@/components/profiles/UserProfiles.vue';
+import ProfilesBtn from '@/components/buttons/ProfilesBtn.vue';
 const emits = defineEmits(['change-component']);
 
 function backToUserProfiles() {
@@ -8,11 +9,17 @@ function backToUserProfiles() {
 </script>
 
 <template>
-  <div>
-    <h1>this is manage profiles</h1>
-
-    <button @click="backToUserProfiles">Done</button>
+  <div :class="classes.manageProfilesWrapper">
+    <h1>Manage Profiles</h1>
+    <div :class="classes.profiles">
+      <!-- -----------Dynamic loaded profiles goes here--------- -->
+    </div>
+    <div :class="classes.ctaWrapper">
+      <ProfilesBtn @click="backToUserProfiles">Done</ProfilesBtn>
+    </div>
   </div>
 </template>
 
-<style lang="scss" module="classes"></style>
+<style lang="scss" module="classes">
+@use '@/sass/components/profiles/manage-profiles';
+</style>
