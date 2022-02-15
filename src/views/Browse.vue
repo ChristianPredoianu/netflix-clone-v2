8dffdba07f4b5cf8ccbb974a959d2bb775b2c484
ChristianPredoianu <50207986+ChristianPredoianu@users.noreply.github.com>
2022-02-15 15:46:28 +0100

    extracted hero content & video controls to own components

--- src/views/Browse.vue
+++ src/views/Browse.vue
@@ -1,58 +1,38 @@
 <script setup>
 import ResponsiveNav from '@/components/navs/ResponsiveNav.vue';
-import VideoBackground from '@/components/ui/VideoBackground.vue';
+import BrowseHeroContent from '@/components/hero/BrowseHeroContent.vue';
+import VideoControls from '@/components/ui/VideoControls.vue';
 import { ref, defineExpose } from 'vue';
 
 const isVideoMuted = ref(true);
-
 const video = ref(null);
 
 defineExpose({ video });
 
-console.log(video);
+function toggleVideoSound(isMuted, isSoundOn) {
+  isVideoMuted.value = isMuted;
+  video.value.muted = isSoundOn;
+}
+
+function replayVideo() {
+  if (video.value.ended) video.value.play();
+}
 </script>
 
 <template>
   <div :class="classes.hero">
     <ResponsiveNav />
-    <VideoBackground ref="video" />
+    <video :class="classes.video" ref="video" muted autoplay>
+      <source src="@/assets/videos/showcase.mp4" type="video/mp4" />
+    </video>
     <div :class="classes.overlay"></div>
     <div class="container">
-      <div :class="classes.heroContent">
-        <div :class="classes.heroHeadings">
-          <h1 :class="classes.heroPrimaryHeading" ref="target">
-            The Jack in the box: Awakening
-          </h1>
-          <h2 :class="classes.heroSecondaryHeading">
-            When a vintage Jack-in-the-box is opened by a dying woman, she
-            enters into a deal with the demon within that would see her illness
-            cured in return for helping it claim six innocent victims.
-          </h2>
-        </div>
-        <div :class="classes.heroCta">
-          <button :class="[classes['btn'], classes['btn--play']]">
-            <span :class="classes.btnSpan"
-              ><font-awesome-icon icon="play" /></span
-            >Play
-          </button>
-          <button :class="[classes['btn'], classes['btn--info']]">
-            <span :class="classes.btnSpan"
-              ><font-awesome-icon icon="info-circle" /></span
-            >MoreInfo
-          </button>
-        </div>
-      </div>
-      <div :class="classes.videoControls">
-        <font-awesome-icon
-          :icon="isVideoMuted ? 'volume-mute' : 'volume-up'"
-          :class="classes.videoControlIcon"
-        />
-
-        <!-- <font-awesome-icon icon="volume-up" :class="classes.videoControlIcon" /> -->
-
-        <font-awesome-icon icon="history" :class="classes.videoControlIcon" />
-        <div :class="classes.recAge">13+</div>
-      </div>
+      <BrowseHeroContent />
+      <VideoControls
+        @toggle-video-sound="toggleVideoSound"
+        @replay-video="replayVideo"
+        :isVideoMuted="isVideoMuted"
+      />
     </div>
   </div>
 </template>
