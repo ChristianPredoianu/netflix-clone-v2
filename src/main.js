a05569b2b621ab551907651da09fa114eef96ad2
ChristianPredoianu <50207986+ChristianPredoianu@users.noreply.github.com>
2022-02-16 15:46:41 +0100

    added on authstatechange for reloading page when signing in

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import { createApp } from 'vue';
 import App from '@/App.vue';
 import router from '@/router/index';
 import store from '@/store';
+import firebase from 'firebase/compat/app';
 import '@/plugins/firebase';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 import addIcons from '@/font-awesome-icons/icons';
@@ -12,8 +13,13 @@ addIcons();
 //This is how to use font awesome
 /* <font-awesome-icon icon="laptop" /> */
 
-createApp(App)
-  .use(router)
-  .use(store)
-  .component('font-awesome-icon', FontAwesomeIcon)
-  .mount('#app');
+let app;
+firebase.auth().onAuthStateChanged(() => {
+  if (!app) {
+    app = createApp(App)
+      .use(router)
+      .use(store)
+      .component('font-awesome-icon', FontAwesomeIcon)
+      .mount('#app');
+  }
+});
