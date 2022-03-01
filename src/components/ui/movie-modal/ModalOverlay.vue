1117df1bfa8e804ed4744dec89612cad76259015
ChristianPredoianu <50207986+ChristianPredoianu@users.noreply.github.com>
2022-03-01 14:52:10 +0100

    added openTrailerOverlay

--- src/components/ui/movie-modal/ModalOverlay.vue
+++ src/components/ui/movie-modal/ModalOverlay.vue
@@ -37,40 +37,39 @@ onMounted(() => {
         </button>
       </div>
     </div>
-    <div :class="classes.modalContainer">
-      <div :class="classes.modalInfo">
-        <div :class="classes.modalInfoLeft">
-          <h3 :class="classes.infoHeading">{{ releaseYear }}</h3>
-          <p :class="classes.movieInfo">{{ props.clickedMovie.overview }}</p>
+
+    <div :class="classes.modalInfo">
+      <div :class="classes.modalInfoLeft">
+        <h3 :class="classes.infoHeading">{{ releaseYear }}</h3>
+        <p :class="classes.movieInfo">{{ props.clickedMovie.overview }}</p>
+      </div>
+      <div :class="classes.modalInfoRight">
+        <div :class="classes.infoList">
+          <h2 :class="classes.infoHeading">Genres</h2>
+          <ul>
+            <li
+              v-for="genre in clickedMovieDetails.genres"
+              :key="genre.index"
+              :class="classes.listItem"
+            >
+              {{ genre.name }}
+            </li>
+          </ul>
         </div>
-        <div :class="classes.modalInfoRight">
-          <div :class="classes.infoList">
-            <h2 :class="classes.infoHeading">Genres</h2>
-            <ul>
-              <li
-                v-for="genre in clickedMovieDetails.genres"
-                :key="genre.index"
-                :class="classes.listItem"
-              >
-                {{ genre.name }}
-              </li>
-            </ul>
-          </div>
-          <div :class="classes.infoList">
-            <h2 :class="classes.infoHeading">Languages</h2>
-            <ul>
-              <li
-                v-for="language in clickedMovieDetails.spoken_languages"
-                :key="language.index"
-                :class="classes.listItem"
-              >
-                {{ language.name }}
-              </li>
-            </ul>
-          </div>
-          <h2 :class="classes.ratingHeading">Rating</h2>
-          <p :class="classes.rating">{{ clickedMovieDetails.vote_average }}</p>
+        <div :class="classes.infoList">
+          <h2 :class="classes.infoHeading">Languages</h2>
+          <ul>
+            <li
+              v-for="language in clickedMovieDetails.spoken_languages"
+              :key="language.index"
+              :class="classes.listItem"
+            >
+              {{ language.name }}
+            </li>
+          </ul>
         </div>
+        <h2 :class="classes.ratingHeading">Rating</h2>
+        <p :class="classes.rating">{{ clickedMovieDetails.vote_average }}</p>
       </div>
     </div>
   </div>
