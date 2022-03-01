8023cdcd8dbb4dd27dd18f61d97b2402dd9e20e8
ChristianPredoianu <50207986+ChristianPredoianu@users.noreply.github.com>
2022-03-01 14:47:34 +0100

    removed console.log

--- src/components/ui/MovieCard.vue
+++ src/components/ui/MovieCard.vue
@@ -1,25 +1,19 @@
 <script setup>
-import { computed } from 'vue';
-import { useStore } from 'vuex';
+import { computed } from "vue";
+import { useStore } from "vuex";
 
 const store = useStore();
-const emit = defineEmits(['onOpenModal']);
+const emit = defineEmits(["onOpenModal"]);
 
 const moviesByGenre = computed(() => store.getters.GET_MOVIES_BY_GENRE);
 
 function openModal(movie) {
-  emit('onOpenModal', { isModalOpen: true, clickedMovie: movie });
+  emit("onOpenModal", { isModalOpen: true, clickedMovie: movie });
 }
-
-console.log(moviesByGenre.value);
 </script>
 
 <template>
-  <div
-    :class="classes.movieCard"
-    v-for="movie in moviesByGenre"
-    :key="movie.id"
-  >
+  <div :class="classes.movieCard" v-for="movie in moviesByGenre" :key="movie.id">
     <img
       :src="`https://image.tmdb.org/t/p/w300${movie.poster_path}`"
       alt="movie poster"
