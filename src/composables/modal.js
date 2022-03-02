23fe3c165ba6ddd90f28fcf2f5050e9054bd705f
ChristianPredoianu <50207986+ChristianPredoianu@users.noreply.github.com>
2022-03-02 11:37:42 +0100

    extracted modal logit into own composable

---
+++ src/composables/modal.js
@@ -0,0 +1,17 @@
+import { ref } from 'vue';
+
+export function useModal() {
+  const isMovieModalOpen = ref(false);
+  const clickedMovie = ref(null);
+
+  function openModal(userClickedMovie) {
+    isMovieModalOpen.value = true;
+    clickedMovie.value = userClickedMovie;
+  }
+
+  function closeModals() {
+    isMovieModalOpen.value = false;
+  }
+
+  return { isMovieModalOpen, clickedMovie, openModal, closeModals };
+}
