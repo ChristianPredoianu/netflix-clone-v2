a3d2748396f03a2b3afd2d73cb305a7e65ab3d94
ChristianPredoianu <50207986+ChristianPredoianu@users.noreply.github.com>
2022-03-03 15:07:33 +0100

    dispatched actions

--- src/views/Profiles.vue
+++ src/views/Profiles.vue
@@ -1,18 +1,28 @@
 <script setup>
-import { shallowRef } from 'vue';
-import LogoNav from '@/components/navs/LogoNav.vue';
-import UserProfiles from '@/components/profiles/UserProfiles.vue';
+import { shallowRef, onMounted, computed } from "vue";
+import { useStore } from "vuex";
+
+import LogoNav from "@/components/navs/LogoNav.vue";
+import UserProfiles from "@/components/profiles/UserProfiles.vue";
 
 let currentComponent = shallowRef(UserProfiles);
+
+const store = useStore();
 
 function changeComponent(comp) {
   currentComponent.value = comp;
 }
+
+onMounted(() => {
+  store.dispatch("SET_CURRENT_USER");
+  store.dispatch("SET_USER_PROFILES_FROM_DB");
+});
 </script>
 
 <template>
   <div :class="classes.profiles">
     <LogoNav />
+
     <div class="container">
       <transition name="fade">
         <component :is="currentComponent" @change-component="changeComponent" />
