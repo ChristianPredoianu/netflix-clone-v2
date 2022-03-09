3dd00ccd1a87e07d806a1814549ecdea981ed901
ChristianPredoianu <50207986+ChristianPredoianu@users.noreply.github.com>
2022-03-09 15:19:20 +0100

    moved singUp from vuex to component

--- src/store/modules/auth.js
+++ src/store/modules/auth.js
@@ -6,8 +6,6 @@ export default {
   state: {
     email: null,
     user: null,
-    error: null,
-    accountMessage: null,
   },
 
   mutations: {
@@ -18,41 +16,11 @@ export default {
     SET_USER(state, payload) {
       state.user = payload;
     },
-
-    SET_ERROR(state, payload) {
-      state.error = payload;
-    },
-
-    SET_ACCOUNT_MESSAGE(state, payload) {
-      state.accountMessage = payload;
-    },
   },
 
   actions: {
     UPDATE_EMAIL({ commit }, payload) {
       commit('UPDATE_EMAIL', payload);
-    },
-
-    signUserUp({ commit }, payload) {
-      firebase
-        .auth()
-        .createUserWithEmailAndPassword(payload.email, payload.password)
-        .then(() => {
-          commit('SET_ERROR', null);
-          commit(
-            'SET_ACCOUNT_MESSAGE',
-            'Account Created. You are being redirected to sign in page'
-          );
-          setTimeout(() => {
-            router.push({ name: 'SignIn' });
-          }, 2000);
-        })
-
-        .catch((error) => {
-          if (error) {
-            commit('SET_ERROR', error.message);
-          }
-        });
     },
 
     SIGN_USER_IN({ commit }, payload) {
