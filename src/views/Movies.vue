e1026a6c94c1560516039a1e930d7fa8f30b729a
ChristianPredoianu <50207986+ChristianPredoianu@users.noreply.github.com>
2022-03-17 13:01:01 +0100

    no change

--- src/views/Movies.vue
+++ src/views/Movies.vue
@@ -1,7 +1,6 @@
 <script setup>
 import { computed } from "vue";
 import { useStore } from "vuex";
-import { useModal } from "@/composables/modal";
 
 import ResponsiveNav from "@/components/navs/ResponsiveNav.vue";
 import GenresFilter from "@/components/filters/GenresFilter.vue";
@@ -9,7 +8,6 @@ import MovieCard from "@/components/ui/MovieCard.vue";
 import MovieModal from "@/components/ui/movie-modal/MovieModal.vue";
 
 const store = useStore();
-const { isMovieModalOpen, clickedMovie, openModal, closeModals } = useModal();
 
 const moviesByGenre = computed(() => store.getters.GET_MOVIES_BY_GENRE);
 </script>
@@ -20,15 +18,10 @@ const moviesByGenre = computed(() => store.getters.GET_MOVIES_BY_GENRE);
     <div class="container">
       <GenresFilter />
       <div :class="classes.movieCardsGrid">
-        <MovieCard :movies="moviesByGenre" @onOpenModal="openModal" />
+        <MovieCard :movies="moviesByGenre" />
       </div>
     </div>
   </main>
-  <MovieModal
-    @onCloseModals="closeModals"
-    v-if="isMovieModalOpen"
-    :clickedMovie="clickedMovie"
-  />
 </template>
 
 <style lang="scss" module="classes">
