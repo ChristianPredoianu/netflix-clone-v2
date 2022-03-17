bdf996b6a423ba293b4bc3d0ce07f09862e15142
ChristianPredoianu <50207986+ChristianPredoianu@users.noreply.github.com>
2022-03-17 13:07:43 +0100

    removed modals

--- src/views/Popular.vue
+++ src/views/Popular.vue
@@ -1,13 +1,11 @@
 <script setup>
 import { computed } from "vue";
 import { useStore } from "vuex";
-import { useModal } from "@/composables/modal";
 import ResponsiveNav from "@/components/navs/ResponsiveNav.vue";
 import MovieCard from "@/components/ui/MovieCard.vue";
 import MovieModal from "@/components/ui/movie-modal/MovieModal.vue";
 
 const store = useStore();
-const { isMovieModalOpen, clickedMovie, openModal, closeModals } = useModal();
 
 const popularMovies = computed(() => store.state.theMovieDB.movieData.popular);
 </script>
@@ -17,15 +15,10 @@ const popularMovies = computed(() => store.state.theMovieDB.movieData.popular);
   <main :class="classes.main">
     <div class="container">
       <div :class="classes.movieCardsGrid">
-        <MovieCard :movies="popularMovies" @onOpenModal="openModal" />
+        <MovieCard :movies="popularMovies" />
       </div>
     </div>
   </main>
-  <MovieModal
-    @onCloseModals="closeModals"
-    v-if="isMovieModalOpen"
-    :clickedMovie="clickedMovie"
-  />
 </template>
 
 <style lang="scss" module="classes">
