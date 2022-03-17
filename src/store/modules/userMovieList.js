b40f33a71333ab6c09eed3254831027fd19c7792
ChristianPredoianu <50207986+ChristianPredoianu@users.noreply.github.com>
2022-03-17 12:54:36 +0100

    added userMovieList vuex module

---
+++ src/store/modules/userMovieList.js
@@ -0,0 +1,39 @@
+import firebase from 'firebase/compat/app';
+
+export default {
+  state: {
+    userMovieList: [],
+  },
+
+  mutations: {
+    SET_USER_MOVIE_LIST_FROM_DB(state, payload) {
+      state.userMovieList = payload;
+    },
+  },
+
+  actions: {
+    SET_USER_MOVIE_LIST_FROM_DB({ commit, rootState }) {
+      const moviesList = [];
+      console.log(rootState.userProfiles.clickedProfile.id);
+      firebase
+        .database()
+        .ref(`users/${rootState.userData.currentUser.id}`)
+        .child(
+          `profiles/${rootState.userProfiles.clickedProfile.id}/moviesList`
+        )
+        .on('value', (snapshot) => {
+          snapshot.forEach((childSnapshot) => {
+            const childData = childSnapshot.val();
+            const id = childData.id;
+
+            const existingProfileIndex = moviesList.findIndex(
+              (profile) => profile.id === id
+            );
+
+            if (existingProfileIndex === -1) moviesList.push(childData);
+            commit('SET_USER_MOVIE_LIST_FROM_DB', moviesList);
+          });
+        });
+    },
+  },
+};
