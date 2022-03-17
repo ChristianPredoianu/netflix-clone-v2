ba63b9fe7c95ad146dab29fd7baec14ef1819e22
ChristianPredoianu <50207986+ChristianPredoianu@users.noreply.github.com>
2022-03-17 12:56:56 +0100

    added user list functionality

--- src/components/ui/MovieCard.vue
+++ src/components/ui/MovieCard.vue
@@ -1,12 +1,23 @@
 <script setup>
-const emit = defineEmits(["onOpenModal"]);
+import { useModal } from "@/composables/modal";
+import { useMovieTrailer } from "@/composables/movieTrailer";
+import { useProfileMovieList } from "@/composables/profileMovieList";
+import MovieModal from "@/components/ui/movie-modal/MovieModal.vue";
+import MovieTrailerOverlay from "@/components/ui/movie-modal/ModalTrailerOverlay.vue";
+import Backdrop from "@/components/ui/movie-modal/Backdrop.vue";
+
 const props = defineProps({
   movies: Object,
 });
 
-function openModal(movie) {
-  emit("onOpenModal", movie);
-}
+const { isMovieModalOpen, clickedMovie, openModal, closeModals } = useModal();
+const {
+  openMovieTrailerModal,
+  closeMovieTrailerModal,
+  isBackdropOpen,
+} = useMovieTrailer();
+
+const { addToProfileList, isMovieInUserList } = useProfileMovieList();
 </script>
 
 <template>
@@ -14,9 +25,31 @@ function openModal(movie) {
     <img
       :src="`https://image.tmdb.org/t/p/w300${movie.poster_path}`"
       alt="movie poster"
-      @click="openModal(movie)"
     />
+
+    <div :class="classes.movieCardIcons">
+      <font-awesome-icon icon="play" @click="openMovieTrailerModal(movie)" />
+      <font-awesome-icon
+        icon="plus-square"
+        @click="addToProfileList(movie)"
+        v-if="!isMovieInUserList(movie)"
+      />
+
+      <font-awesome-icon icon="times-circle" v-else />
+      <font-awesome-icon icon="info-circle" @click="openModal(movie)" />
+    </div>
   </div>
+
+  <MovieModal
+    @onCloseModals="closeModals"
+    v-if="isMovieModalOpen"
+    :clickedMovie="clickedMovie"
+  />
+  <Backdrop v-if="isBackdropOpen" @onCloseModals="closeMovieTrailerModal" />
+  <MovieTrailerOverlay
+    v-if="isBackdropOpen"
+    @onCloseMovieTrailerOverlay="closeMovieTrailerModal"
+  />
 </template>
 
 <style lang="scss" module="classes">
