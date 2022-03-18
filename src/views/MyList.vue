23f7f66a3f181b9e7646e4f07052434a30b4d3ed
ChristianPredoianu <50207986+ChristianPredoianu@users.noreply.github.com>
2022-03-18 11:48:54 +0100

    added moviecard component

--- src/views/MyList.vue
+++ src/views/MyList.vue
@@ -1,7 +1,23 @@
-<script setup></script>
+<script setup>
+import { computed } from "vue";
+import { useStore } from "vuex";
+import ResponsiveNav from "@/components/navs/ResponsiveNav.vue";
+import MovieCard from "@/components/ui/MovieCard.vue";
+
+const store = useStore();
+const userMovieList = computed(() => store.state.userMovieList.userMovieList);
+console.log(userMovieList.value);
+</script>
 
 <template>
-  <div>this is mylist</div>
+  <ResponsiveNav />
+  <main :class="classes.main">
+    <div class="container">
+      <div :class="classes.movieCardsGrid">
+        <MovieCard :movies="userMovieList" />
+      </div>
+    </div>
+  </main>
 </template>
 
 <style lang="scss" module="classes">
