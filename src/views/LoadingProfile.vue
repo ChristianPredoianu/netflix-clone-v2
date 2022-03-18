59909626fc19db2b2f5e65eebe5311736b14ee45
ChristianPredoianu <50207986+ChristianPredoianu@users.noreply.github.com>
2022-03-18 11:48:25 +0100

    added loading-spinner view

--- src/views/LoadingProfile.vue
+++ src/views/LoadingProfile.vue
@@ -1,11 +1,24 @@
 <script setup>
+import { onMounted } from "vue";
+import { useRouter } from "vue-router";
 import LogoNav from "@/components/navs/LogoNav.vue";
+
+const router = useRouter();
+
+onMounted(() => {
+  setTimeout(() => {
+    router.push({ name: "Browse" });
+  }, 2000);
+});
 </script>
 
 <template>
-  <div>
-    <LogoNav />
-  </div>
+  <LogoNav />
+  <main :class="classes.main">
+    <div :class="classes.loader"></div>
+  </main>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" module="classes">
+@use '@/sass/views/loading-profile';
+</style>
