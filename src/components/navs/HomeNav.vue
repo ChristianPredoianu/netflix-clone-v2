d2f89b3c706bc2945c233ae09d6288f83f8f6418
ChristianPredoianu <50207986+ChristianPredoianu@users.noreply.github.com>
2022-03-22 12:14:55 +0100

    removed div

--- src/components/navs/HomeNav.vue
+++ src/components/navs/HomeNav.vue
@@ -1,17 +1,15 @@
 <script setup>
-import NetflixLogo from '@/components/ui/NetflixLogo.vue';
-import SignInBtn from '@/components/buttons/SignInBtn.vue';
+import NetflixLogo from "@/components/ui/NetflixLogo.vue";
+import SignInBtn from "@/components/buttons/SignInBtn.vue";
 </script>
 
 <template>
-  <div>
-    <header :class="classes.header">
-      <nav :class="classes.nav">
-        <NetflixLogo />
-        <SignInBtn>Sign In</SignInBtn>
-      </nav>
-    </header>
-  </div>
+  <header :class="classes.header">
+    <nav :class="classes.nav">
+      <NetflixLogo />
+      <SignInBtn>Sign In</SignInBtn>
+    </nav>
+  </header>
 </template>
 
 <style lang="scss" module="classes">
