e15a24d68465f04b5e870213703972d33833f9d3
ChristianPredoianu <50207986+ChristianPredoianu@users.noreply.github.com>
2022-03-22 12:15:45 +0100

    moved heroNav outside hero section + removed div

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -9,86 +9,84 @@ import Footer from "@/components/footer/Footer.vue";
 </script>
 
 <template>
-  <div>
-    <section :class="classes.hero">
-      <div class="container">
-        <HomeNav />
-        <HeroContent />
-      </div>
-    </section>
-    <HomeSection>
-      <HomeSectionHeadings
-        heading="Watch on your tv"
-        subHeading=" Watch on smart-tv:s, Playstation, Xbox, Chromecast, Apple TV,
+  <HomeNav />
+  <section :class="classes.hero">
+    <div class="container">
+      <HeroContent />
+    </div>
+  </section>
+  <HomeSection>
+    <HomeSectionHeadings
+      heading="Watch on your tv"
+      subHeading=" Watch on smart-tv:s, Playstation, Xbox, Chromecast, Apple TV,
               Blu-ray-players and many more."
+    />
+    <div :class="classes.imgContainer">
+      <img src="@/assets/images/tv2.png" alt="tv" :class="classes.outsideImg" />
+      <video
+        src="@/assets/videos/video1.mp4"
+        playsinline
+        autoplay
+        muted
+        loop
+        :class="classes.inside"
+      ></video>
+    </div>
+  </HomeSection>
+  <HomeSection>
+    <div :class="classes.imgContainer">
+      <img src="@/assets/images/phone.png" alt="tv" :class="classes.phoneOutside" />
+      <img
+        src="@/assets/images/netflix-phone.jpg"
+        playsinline
+        autoplay
+        muted
+        loop
+        :class="classes.phoneInside"
       />
-      <div :class="classes.imgContainer">
-        <img src="@/assets/images/tv2.png" alt="tv" :class="classes.outsideImg" />
-        <video
-          src="@/assets/videos/video1.mp4"
-          playsinline
-          autoplay
-          muted
-          loop
-          :class="classes.inside"
-        ></video>
-      </div>
-    </HomeSection>
-    <HomeSection>
-      <div :class="classes.imgContainer">
-        <img src="@/assets/images/phone.png" alt="tv" :class="classes.phoneOutside" />
-        <img
-          src="@/assets/images/netflix-phone.jpg"
-          playsinline
-          autoplay
-          muted
-          loop
-          :class="classes.phoneInside"
-        />
-      </div>
-      <HomeSectionHeadings
-        heading="Download your programmes to watch offline."
-        subHeading="Save your favourites so that you always have something to watch."
+    </div>
+    <HomeSectionHeadings
+      heading="Download your programmes to watch offline."
+      subHeading="Save your favourites so that you always have something to watch."
+    />
+  </HomeSection>
+  <HomeSection>
+    <HomeSectionHeadings
+      heading="Watch anywhere."
+      subHeading="Stream unlimited movies and series on mobile, tablet, pc and tv without paying extra."
+    />
+    <div :class="classes.imgContainer">
+      <img src="@/assets/images/imac.png" alt="tv" :class="classes.outsideImg" />
+      <video
+        src="@/assets/videos/video2.mp4"
+        playsinline
+        autoplay
+        muted
+        loop
+        :class="classes.insideImac"
+      ></video>
+      <img
+        src="@/assets/images/iphone-netflix.png"
+        alt="tv"
+        :class="classes.imacOutsideIphone"
       />
-    </HomeSection>
-    <HomeSection>
-      <HomeSectionHeadings
-        heading="Watch anywhere."
-        subHeading="Stream unlimited movies and series on mobile, tablet, pc and tv without paying extra."
-      />
-      <div :class="classes.imgContainer">
-        <img src="@/assets/images/imac.png" alt="tv" :class="classes.outsideImg" />
-        <video
-          src="@/assets/videos/video2.mp4"
-          playsinline
-          autoplay
-          muted
-          loop
-          :class="classes.insideImac"
-        ></video>
-        <img
-          src="@/assets/images/iphone-netflix.png"
-          alt="tv"
-          :class="classes.imacOutsideIphone"
-        />
+    </div>
+  </HomeSection>
+  <HomeSection>
+    <div :class="classes.faqWrapper">
+      <h3 :class="classes.accordionHeading">FAQ</h3>
+      <BaseAccordion />
+      <h3 :class="classes.formHeading">
+        Ready to start watching? Fill in your e-mail to create an account.
+      </h3>
+      <div :class="classes.faqFormWrapper">
+        <GetStartedForm />
       </div>
-    </HomeSection>
-    <HomeSection>
-      <div :class="classes.faqWrapper">
-        <h3 :class="classes.accordionHeading">FAQ</h3>
-        <BaseAccordion />
-        <h3 :class="classes.formHeading">
-          Ready to start watching? Fill in your e-mail to create an account.
-        </h3>
-        <div :class="classes.faqFormWrapper">
-          <GetStartedForm />
-        </div>
-      </div>
-    </HomeSection>
-    <HomeSection>
-      <Footer />
-    </HomeSection>
-  </div>
+    </div>
+  </HomeSection>
+  <HomeSection>
+    <Footer />
+  </HomeSection>
 </template>
 
 <style lang="scss" module="classes">
