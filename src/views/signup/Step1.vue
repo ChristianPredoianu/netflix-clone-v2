b8f6eff636e867d6778c2a2275da4abd4873bd79
ChristianPredoianu <50207986+ChristianPredoianu@users.noreply.github.com>
2022-03-22 12:34:18 +0100

    removed div

--- src/views/signup/Step1.vue
+++ src/views/signup/Step1.vue
@@ -1,38 +1,36 @@
 <script setup>
-import SignUpNav from '@/components/navs/SignUpNav.vue';
-import ContinueBtn from '@/components/buttons/ContinueBtn.vue';
-import router from '../../router';
+import SignUpNav from "@/components/navs/SignUpNav.vue";
+import ContinueBtn from "@/components/buttons/ContinueBtn.vue";
+import router from "../../router";
 
 function goToStepTwoHandler() {
-  router.push({ name: 'SignupStep2' });
+  router.push({ name: "SignupStep2" });
 }
 </script>
 
 <template>
-  <div>
-    <SignUpNav />
-    <div class="container">
-      <main :class="classes.setUpAccount">
-        <div :class="classes.icons">
-          <font-awesome-icon icon="laptop" :class="classes.icon" />
-          <font-awesome-icon icon="tv" :class="classes.icon" />
-          <font-awesome-icon icon="mobile-alt" :class="classes.icon" />
-        </div>
-        <div>
-          <p :class="classes.infoParagraph">
-            Step <span :class="classes.span">1</span> of
-            <span :class="classes.span">2</span>
-          </p>
-          <h1 :class="classes.infoHeading">Finish setting up your account</h1>
-          <h2 :class="classes.infoHeadingSecondary">
-            Netflix is personalised for you. Create a password to watch Netflix
-            on any device at any time.
-          </h2>
+  <SignUpNav />
+  <div class="container">
+    <main :class="classes.setUpAccount">
+      <div :class="classes.icons">
+        <font-awesome-icon icon="laptop" :class="classes.icon" />
+        <font-awesome-icon icon="tv" :class="classes.icon" />
+        <font-awesome-icon icon="mobile-alt" :class="classes.icon" />
+      </div>
+      <div>
+        <p :class="classes.infoParagraph">
+          Step <span :class="classes.span">1</span> of
+          <span :class="classes.span">2</span>
+        </p>
+        <h1 :class="classes.infoHeading">Finish setting up your account</h1>
+        <h2 :class="classes.infoHeadingSecondary">
+          Netflix is personalised for you. Create a password to watch Netflix on any
+          device at any time.
+        </h2>
 
-          <ContinueBtn @click="goToStepTwoHandler">Continue</ContinueBtn>
-        </div>
-      </main>
-    </div>
+        <ContinueBtn @click="goToStepTwoHandler">Continue</ContinueBtn>
+      </div>
+    </main>
   </div>
 </template>
 
