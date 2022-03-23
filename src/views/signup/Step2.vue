f0c16345f8bae950efaa35f261a11e2b19f3235a
ChristianPredoianu <50207986+ChristianPredoianu@users.noreply.github.com>
2022-03-23 11:19:53 +0100

    removed div

--- src/views/signup/Step2.vue
+++ src/views/signup/Step2.vue
@@ -8,17 +8,16 @@ import SignUpForm from '@/components/forms/SignUpForm.vue';
   <SignUpNav />
   <div class="container">
     <main :class="classes.setUpAccount">
-  
-        <p :class="classes.infoParagraph">
-          Step <span :class="classes.span">2</span> of
-          <span :class="classes.span">2</span>
-        </p>
-        <h1 :class="classes.infoHeading">Finish setting up your account</h1>
-        <h2 :class="classes.infoHeadingSecondary">
-          Netflix is personalised for you. Create a password to watch Netflix on
-          any device at any time.
-        </h2>
-     
+      <p :class="classes.infoParagraph">
+        Step <span :class="classes.span">2</span> of
+        <span :class="classes.span">2</span>
+      </p>
+      <h1 :class="classes.infoHeading">Finish setting up your account</h1>
+      <h2 :class="classes.infoHeadingSecondary">
+        Netflix is personalised for you. Create a password to watch Netflix on
+        any device at any time.
+      </h2>
+
       <div :class="classes.signUpForm">
         <SignUpForm />
       </div>
