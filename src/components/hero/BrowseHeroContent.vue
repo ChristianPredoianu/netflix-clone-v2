6b40849e6b8a5f84885350f17aee2ad434a9bf4e
ChristianPredoianu <50207986+ChristianPredoianu@users.noreply.github.com>
2022-03-23 12:28:49 +0100

    removed script tag

--- src/components/hero/BrowseHeroContent.vue
+++ src/components/hero/BrowseHeroContent.vue
@@ -1,25 +1,19 @@
-<script setup></script>
-
 <template>
   <div :class="classes.heroContent">
     <div :class="classes.heroHeadings">
-      <h1 :class="classes.heroPrimaryHeading">
-        The Jack in the box: Awakening
-      </h1>
+      <h1 :class="classes.heroPrimaryHeading">The Jack in the box: Awakening</h1>
       <h2 :class="classes.heroSecondaryHeading">
-        When a vintage Jack-in-the-box is opened by a dying woman, she enters
-        into a deal with the demon within that would see her illness cured in
-        return for helping it claim six innocent victims.
+        When a vintage Jack-in-the-box is opened by a dying woman, she enters into a deal
+        with the demon within that would see her illness cured in return for helping it
+        claim six innocent victims.
       </h2>
     </div>
     <div :class="classes.heroCta">
       <button :class="[classes['btn'], classes['btn--play']]">
-        <span :class="classes.btnSpan"><font-awesome-icon icon="play" /></span
-        >Play
+        <span :class="classes.btnSpan"><font-awesome-icon icon="play" /></span>Play
       </button>
       <button :class="[classes['btn'], classes['btn--info']]">
-        <span :class="classes.btnSpan"
-          ><font-awesome-icon icon="info-circle" /></span
+        <span :class="classes.btnSpan"><font-awesome-icon icon="info-circle" /></span
         >MoreInfo
       </button>
     </div>
