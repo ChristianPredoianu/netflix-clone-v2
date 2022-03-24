3ef67ce5600f8616b76afb8d4ef9deb0def008c7
ChristianPredoianu <50207986+ChristianPredoianu@users.noreply.github.com>
2022-03-24 13:53:32 +0100

    extracted responsive nav links here

---
+++ src/components/navs/ResponsiveNavLinks.vue
@@ -0,0 +1,30 @@
+<template>
+  <router-link
+    :to="{ name: 'Browse' }"
+    :active-class="classes.active"
+    :class="classes.navLinksItem"
+    >Home</router-link
+  >
+  <router-link
+    :to="{ name: 'Movies' }"
+    :active-class="classes.active"
+    :class="classes.navLinksItem"
+    >Movies</router-link
+  >
+  <router-link
+    :to="{ name: 'Popular' }"
+    :active-class="classes.active"
+    :class="classes.navLinksItem"
+    >Popular</router-link
+  >
+  <router-link
+    :to="{ name: 'MyList' }"
+    :active-class="classes.active"
+    :class="classes.navLinksItem"
+    >My list</router-link
+  >
+</template>
+
+<style lang="scss" module="classes">
+@use '@/sass/components/navs/responsive-nav-links';
+</style>
