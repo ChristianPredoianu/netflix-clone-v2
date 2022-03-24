541515fa03d47a13b5e2c8987d8213d4b5cb4ba7
ChristianPredoianu <50207986+ChristianPredoianu@users.noreply.github.com>
2022-03-24 14:09:43 +0100

    added navigateHome. If user signer in navigate to browse else navigate home

--- src/components/ui/NetflixLogo.vue
+++ src/components/ui/NetflixLogo.vue
@@ -1,8 +1,24 @@
+<script setup>
+import { computed } from "vue";
+import { useRouter } from "vue-router";
+import { useStore } from "vuex";
+
+const router = useRouter();
+const store = useStore();
+
+const currentUser = computed(() => store.state.userData.currentUser);
+
+function navigateHome() {
+  !currentUser.value.id ? router.push({ name: "Home" }) : router.push({ name: "Browse" });
+}
+</script>
+
 <template>
   <img
     src="@/assets/images/netclone.png"
     :class="classes.netflixLogo"
     alt="Netclone logo"
+    @click="navigateHome"
   />
 </template>
 
