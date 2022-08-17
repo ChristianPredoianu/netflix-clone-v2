f69ddd14debef498dbfed1b3e08fd38f542294f7
Christian Predoianu <christianpredoianu@yahoo.com>
2022-08-17 10:02:21 +0200

    changed popularMovies to get the right movies from store

--- src/views/Popular.vue
+++ src/views/Popular.vue
@@ -1,18 +1,20 @@
 <script setup>
-import { computed } from "vue";
-import { useStore } from "vuex";
-import { useSearchMovie } from "@/composables/searchMovie";
-import ResponsiveNav from "@/components/navs/ResponsiveNav.vue";
-import MovieCard from "@/components/ui/MovieCard.vue";
-import MovieModal from "@/components/ui/movie-modal/MovieModal.vue";
-import NetflixBorder from "@/components/ui/NetflixBorder.vue";
-import Footer from "@/components/footer/Footer.vue";
+import { computed } from 'vue';
+import { useStore } from 'vuex';
+import { useSearchMovie } from '@/composables/searchMovie';
+import ResponsiveNav from '@/components/navs/ResponsiveNav.vue';
+import MovieCard from '@/components/ui/MovieCard.vue';
+import MovieModal from '@/components/ui/movie-modal/MovieModal.vue';
+import NetflixBorder from '@/components/ui/NetflixBorder.vue';
+import Footer from '@/components/footer/Footer.vue';
 
 const store = useStore();
 
 const { searchTerm, setSearchTerm, searchMovie } = useSearchMovie();
 
-const popularMovies = computed(() => store.state.theMovieDB.movieData.popular);
+const popularMovies = computed(
+  () => store.state.theMovieDB.movieData.popular.movies
+);
 </script>
 
 <template>
@@ -20,7 +22,9 @@ const popularMovies = computed(() => store.state.theMovieDB.movieData.popular);
   <main :class="classes.main">
     <div class="container">
       <div :class="classes.movieCardsGrid">
-        <MovieCard :movies="!searchTerm ? popularMovies : searchMovie(popularMovies)" />
+        <MovieCard
+          :movies="!searchTerm ? popularMovies : searchMovie(popularMovies)"
+        />
       </div>
     </div>
   </main>
