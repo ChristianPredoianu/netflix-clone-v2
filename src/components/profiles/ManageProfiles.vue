22c3da24768b5725c0b875a6080ef3c0aece4108
Christian Predoianu <christianpredoianu@yahoo.com>
2022-08-21 09:15:28 +0200

    extracted caz<rd logit to ManageProfileCard component

--- src/components/profiles/ManageProfiles.vue
+++ src/components/profiles/ManageProfiles.vue
@@ -3,12 +3,15 @@ import { computed } from 'vue';
 import { useStore } from 'vuex';
 import EditProfiles from '@/components/profiles/EditProfiles.vue';
 import UserProfiles from '@/components/profiles/UserProfiles.vue';
+import ManageProfileCard from '@/components/cards/ManageProfileCard.vue';
 import ProfilesBtn from '@/components/buttons/ProfilesBtn.vue';
+
 const emits = defineEmits(['change-component']);
+
 const store = useStore();
 
 const userProfiles = computed(() => store.state.userProfiles.userProfiles);
-console.log(userProfiles.value);
+
 function goToEditProfile(profile) {
   store.dispatch('SET_CLICKED_PROFILE', profile);
   emits('change-component', EditProfiles);
@@ -23,16 +26,11 @@ function componentChange() {
   <div :class="classes.userProfilesWrapper">
     <h1 :class="classes.profilesHeading">Manage Profiles</h1>
     <div :class="classes.profiles">
-      <div
-        :class="classes.profileCard"
+      <ManageProfileCard
         v-for="profile in userProfiles"
         :key="profile.id"
-        @click="goToEditProfile(profile)"
-      >
-        <font-awesome-icon :icon="profile.icon" :class="classes.profileIcon" />
-        <p :class="classes.profileName">{{ profile.name }}</p>
-        <font-awesome-icon icon="edit" :class="classes.editProfileIcon" />
-      </div>
+        :profile="profile"
+      />
     </div>
     <div>
       <ProfilesBtn @click="componentChange">Back</ProfilesBtn>
