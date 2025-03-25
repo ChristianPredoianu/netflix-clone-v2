83c36de25012b480667e4a2b960f8f5d963e7166
Christian Predoianu <christianpredoianu@yahoo.com>
2025-03-25 13:46:39 +0100

    deleted

--- src/store/url.js
+++
@@ -1,9 +0,0 @@
-export const url = 'https://api.themoviedb.org/3/movie/';
-
-export const apiKey = `?api_key=${import.meta.env.VITE_APP_API_KEY}`;
-
-export const categoryUrl = 'https://api.themoviedb.org/3/discover/movie';
-('&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false');
-
-export const query =
-  '&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false';
